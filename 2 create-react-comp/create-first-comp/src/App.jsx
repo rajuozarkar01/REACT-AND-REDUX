ab1a229c90c49f627af026a96e317fada1b5f373
Raju Ozarkar <rajuozarkar01@gmail.com>
2024-09-01 16:34:25 +0530

    export default, import , component

--- 2 create-react-comp/create-first-comp/src/App.jsx
+++ 2 create-react-comp/create-first-comp/src/App.jsx
@@ -1,4 +1,9 @@
-function App(){
-
+import RjButton from "./RjButton";
+function App() {
+  return <div>
+      <h1>This is the debue to react</h1>
+      <RjButton></RjButton>
+  </div>
+  
 }
 export default App;
---
+++ 2 create-react-comp/create-first-comp/src/RjButton.jsx
@@ -0,0 +1,5 @@
+
+function RjButton() {
+  return <button>Click Me</button>;
+}
+export default RjButton;

adb0e2ff99cb7b732d9cd15ce9730611111345ca
Raju Ozarkar <rajuozarkar01@gmail.com>
2024-09-04 11:01:02 +0530

    css dosen't apply

--- Projects/1-todo-app-version/src/App.jsx
+++ Projects/1-todo-app-version/src/App.jsx
@@ -3,16 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
-import TodoItem1 from "./components/TodoItem2";
+import TodoItem1 from "./components/TodoItem1";
 import TodoItem2 from "./components/TodoItem2";
+import "./App.css";
 function App() {
   return (
     <center class="todo-container">
       <AppName/>
       <AddTodo/>
       <TodoItem1/>
-      <TodoItem2 />
-
+      <TodoItem2/>
     </center>
   );
 }

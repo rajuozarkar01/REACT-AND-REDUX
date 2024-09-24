4565cf897d86cc42f29be88e6bc31f04b148d3b1
Raju Ozarkar <rajuozarkar01@gmail.com>
2024-09-24 10:39:18 +0530

    Item, FoodInput added to Healthy Food App

--- 18 Fragments/learning-Fragment/src/App.jsx
+++ 18 Fragments/learning-Fragment/src/App.jsx
@@ -1,21 +1,49 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import Container from "./components/Container";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import FoodInput from "./components/FoodInput";
+import { useState } from "react";
 
 function App() {
-  let foodItems = ["Green Vegetables", "Milk", "Fruits And Nuts", "Roti","Rice", "Wine","Samosa"];
+  // let foodItems = [
+  //   "Green Vegetables",
+  //   "Milk",
+  //   "Fruits And Nuts",
+  //   "Roti",
+  //   "Rice",
+  //   "Wine",
+  //   "Samosa",
+  // ];
+
+  // let textStateArr = useState("Food Item Entered by user");
+  // let textToShow = textStateArr[0];
+  // let setTextState = textStateArr[1];
+
+  let [foodItems, setFoodItems] = useState([]);
+
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
+      let newFoodItem = event.target.value;
+      event.target.value = "";
+      let newItems = [...foodItems, newFoodItem];
+      setFoodItems(newItems);
+      console.log("Food value enterd is " + newFoodItem);
+    }
+  };
 
   return (
-  <>
-    <Container>
-    <Container><h1 className="dynamic">Healthy Food</h1></Container>
-    <ErrorMessage items = {foodItems}></ErrorMessage>
-    <FoodItems items = {foodItems}></FoodItems>
-    </Container>
-    <Container><p>"Good food, good mood, good for you." "Eating well is a form of self-respect." "Nourishing my body and soul." "Healthy choices, happy vibes." </p></Container>
-  </>
+    <>
+      <Container>
+        <h1 className="app-heading">Healthy Food</h1>
+        <FoodInput handleKeyDown={onKeyDown}></FoodInput>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        {/* <p>{textToShow}</p> */}
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+      {/* <Container><p>"Good food, good mood, good for you." "Eating well is a form of self-respect." "Nourishing my body and soul." "Healthy choices, happy vibes." </p></Container> */}
+    </>
   );
 }
 
--- 18 Fragments/learning-Fragment/src/components/Item.jsx
+++ 18 Fragments/learning-Fragment/src/components/Item.jsx
@@ -1,8 +1,15 @@
-// import styles from "./Item.module.css";
-const Item = ({ foodItem }) => {
+import styles from "./Item.module.css";
+const Item = ({ foodItem, handleBuyButton }) => {
+  
   return (
     <li className="dynamic list-group-item">
       <span className="rj-span">{foodItem}</span>
+      <button
+        className={`${styles.button} btn btn-info`}
+        onClick={handleBuyButton}
+      >
+        Buy
+      </button>
     </li>
   );
 };

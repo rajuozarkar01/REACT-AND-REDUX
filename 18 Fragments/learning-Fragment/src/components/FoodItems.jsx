774a7c598a9473c3737316dff074d011adae2035
Raju Ozarkar <rajuozarkar01@gmail.com>
2024-09-28 20:21:21 +0530

    $$$

--- 18 Fragments/learning-Fragment/src/components/FoodItems.jsx
+++ 18 Fragments/learning-Fragment/src/components/FoodItems.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import Item from "./Item";
 
 const FoodItems = ({ items }) => {
+  let [activeItems, setActiveItems] = useState([]);
+  let onBuyButton = (item, event) => {
+    let newItems = [...activeItems, event, item];
+    setActiveItems(newItems);
+  };
   return (
-    <ul className="list-group">
+    <ul className="list-group ">
       {items.map((item) => (
         <Item
           key={item}
           foodItem={item}
-          handleBuyButton={() => console.log(`${item} bought`)}
+          bought={activeItems.includes(item)}
+          handleBuyButton={(event) => onBuyButton(item, event)}
         ></Item>
       ))}
     </ul>
--- 18 Fragments/learning-Fragment/src/components/Item.jsx
+++ 18 Fragments/learning-Fragment/src/components/Item.jsx
@@ -1,9 +1,10 @@
 import styles from "./Item.module.css";
-const Item = ({ foodItem, handleBuyButton }) => {
-  
+const Item = ({ foodItem, bought, handleBuyButton }) => {
   return (
-    <li className="dynamic list-group-item">
-      <span className="rj-span">{foodItem}</span>
+    <li
+      className={`${styles["rj-item"]} list-group-item ${bought && "active"}`}
+    >
+      <span className={styles["rj-span"]}>{foodItem}</span>
       <button
         className={`${styles.button} btn btn-info`}
         onClick={handleBuyButton}

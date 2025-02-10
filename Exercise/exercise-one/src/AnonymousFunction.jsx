1ed55046f58b25f14c5cf7c1a903545c1fd1f02e
Raju Ozarkar <rajuozarkar01@gmail.com>
2025-02-10 10:37:30 +0530

    .gitingnore to Execise/exercise-one

--- Exercise/exercise-one/src/AnonymousFunction.jsx
+++
@@ -1,7 +0,0 @@
-const AnonymousFunction = ({ str }) => {
-  return <p>{`Hi I passed array as object and addition of a + b = ${str}`}</p>;
-};
-export default AnonymousFunction;
-
-
-// dosomething(()=>{console.log()});
--- Exercise/exercise-one/src/Hello.jsx
+++
@@ -1,7 +0,0 @@
-function Hello(){
-  let nameStr = "Raju";
-  let lastName = "ozarkar";
-  let age = 42;
-  return <p>{`This is my name: ${nameStr} and last name: ${lastName} also my age is : ${age}`}</p>
-}
-export default Hello;
